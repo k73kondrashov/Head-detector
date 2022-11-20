c2a6e98fad30a42ec8f24163bf107fb99d8237c6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-20 17:05:42 +0000

    Adding tarfile member sanitization to extractall()

--- train.py
+++ train.py
@@ -201,7 +201,26 @@ def main():
         tar_name = model_url.split('/')[-1]
         with tarfile.open(tar_name, 'r:gz') as tar:
             print('\nРаспаковка модели')
-            tar.extractall('pretrained_models')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, "pretrained_models")
             untar_name = tar_name.split('.')[0]
             os.rename(f'pretrained_models/{untar_name}', f'pretrained_models/{model_name}')
         print(f'Модель распакована в pretrained_models/{model_name}')
